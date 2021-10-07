fcb9a514e445fb5c307934230e01588e1aff1145
Artur Dębkowski <arturdebkowski@gmail.com>
2021-10-07 12:03:32 +0200

    remove debug log

--- Assets/CubeRotator.cs
+++ Assets/CubeRotator.cs
@@ -4,13 +4,6 @@ using UnityEngine;
 
 public class CubeRotator : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        Debug.Log("Hello world!");
-    }
-
-    // Update is called once per frame
     void Update()
     {
         transform.Rotate(0, 1, 0);
